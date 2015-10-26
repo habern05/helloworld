2deebc8a4e92ca4a7d9865559eeaa97fba151de5
harald051187 <harald.bernhard@gmx.at>
2015-10-26 13:53:13 +0100

    Update main.java

--- Aufgabe-1/main.java
+++ Aufgabe-1/main.java
@@ -1,1 +1,9 @@
-
+class main {
+public static void main (String []args){
+System.out.println("Hello World");
+    System.out.println("Hallo Welt");
+    System.out.println("Hy");
+    System.out.println("Hy");
+    System.out.println("Hallo Welt");
+}
+}

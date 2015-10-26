13998bfd6e4c8e23990b7cdbe8a42a620c1e498f
harald051187 <harald.bernhard@gmx.at>
2015-10-26 13:50:05 +0100

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-class main {
-public static void main (String []args){
-System.out.println("Hello World");
-    System.out.println("Hallo Welt");
-    System.out.println("Hy");
-    System.out.println("Hy");
-    System.out.println("Hallo Welt");
-}
-}

f56317335412fbd99467a8fcae68cc52dc4eed47
Harald Bernhard <Harald Bernhard>
2015-10-26 13:41:27 +0100

    inter

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ public static void main (String []args){
 System.out.println("Hello World");
     System.out.println("Hallo Welt");
     System.out.println("Hy");
+    System.out.println("Hy");
     System.out.println("Hallo Welt");
 }
 }

eb64b970cf6e68af79f9c99280eee3927c1dd813
Harald Bernhard <Harald Bernhard>
2015-10-26 13:58:22 +0100

    internationalisiert

--- Aufgabe-1/main.java
+++ Aufgabe-1/main.java
@@ -4,6 +4,7 @@ System.out.println("Hello World");
     System.out.println("Hallo Welt");
     System.out.println("Hy");
     System.out.println("Hy");
+    System.out.println("Hi")
     System.out.println("Hallo Welt");
 }
 }

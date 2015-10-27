e7424bf0a2e680b1f24be02f4c941dff67b1c090
harald051187 <harald.bernhard@gmx.at>
2015-10-27 16:43:27 +0100

    debugging

--- Aufgabe-1/Haupt.java
+++ Aufgabe-1/Haupt.java
@@ -1,10 +1,10 @@
-class main {
+class Haupt {
 public static void main (String []args){
 System.out.println("Hello World");
     System.out.println("Hallo Welt");
     System.out.println("Hy");
     System.out.println("Hy");
-    System.out.println("Hi")
+    System.out.println("Hi");
     System.out.println("Hallo Welt");
 }
 }
